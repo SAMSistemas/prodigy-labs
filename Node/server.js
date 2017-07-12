7f8b06fe7ede27c7c6e7bc7f5639bf84f540fa27
elang <eric.lang@samsistemas.com.ar>
2017-07-12 15:47:36 -0300

    Initial Commit

--- Node/server.js
+++ Node/server.js
@@ -1,16 +1,52 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
-import mock from './mock_data';
+import mock, { dataWrap } from './mock_data';
 
+const team = mock["team"];
+
+/* Obtengo una instancia de express */
 const app = express();
 
-app.get("*", (request, response) => {
-    const { path } = request;
-    const pathName = path.split("/")[1];
-    response.json(mock[pathName]);
+/* Indico a express que use el middleware morgan,
+este middleware se encarga de loggear cada request que llega al server */
+app.use(morgan());
+
+/* Indico que express usa el middleware bodyParser para procesar el body del request */
+app.use(bodyParser());
+
+app.get("/team", (request, response) => response.json(dataWrap(team)));
+
+app.post("/team", (request, response) => {
+    const { body } = request;
+    team.push(body);
+
+    response.status(201).json(dataWrap({ok: true}));
+
 });
 
+app.get("/team/:id", (request, response) => {
+    const { params: { id } } = request;
 
-app.listen(9001, () => {
-    console.log("Sever started.");
+    if(!team.hasOwnProperty(id)) {
+        return response.status(400).json({error: "Ese miembro no existe!!"});
+    }
+
+    response.json(dataWrap(team[id]));
 });
+
+app.delete("/team/:id", (request, response) => {
+    const { params: { id } } = request;
+
+    if(!team.hasOwnProperty(id)) {
+        return response.status(400).json({error: "Ese miembro no existe!!"});
+    }
+    /* Hace un splice de un lugar desde la posición indicada */
+    team.splice(id, 1);
+
+    response.json(dataWrap({ok: true}));
+
+});
+
+app.listen(9001, _ => console.log("Server started."));
